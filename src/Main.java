5719fabe7d46ee493dba5b403c91f37a0bd5c931
sanjiv-0103 <bp2411@srmist.edu.in>
2026-03-10 13:54:19 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
+}
